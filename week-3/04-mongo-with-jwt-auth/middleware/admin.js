82d2012a454e5212ea526ffaec4027d427463785
nikunjmathur08 <nikunjmathur08@users.noreply.github.com>
2024-07-05 13:51:32 +0530

    Added 04-mongo-with-jwt-auth

--- week-3/04-mongo-with-jwt-auth/middleware/admin.js
+++ week-3/04-mongo-with-jwt-auth/middleware/admin.js
@@ -1,7 +1,26 @@
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = "1234";
 // Middleware for handling auth
-function adminMiddleware(req, res, next) {
-    // Implement admin auth logic
-    // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+async function adminMiddleware(req, res, next) {
+  // Implement admin auth logic
+  // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+  const token = req?.headers?.authorization?.split(" ")[1];
+  if (!token) {
+    return res.status(401).send({ message: "Unauthorized" });
+  }
+  try {
+    jwt.verify(token, JWT_SECRET, (err, data) => {
+      if (err) {
+        return res.status(401).send({ message: "Unauthorized" });
+      }
+      req._id = data._id;
+      next();
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .send({ message: "Error verying token", error: error });
+  }
 }
 
 module.exports = adminMiddleware;
