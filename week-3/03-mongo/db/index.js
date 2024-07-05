97853d6236e787d700b30de8e91725e5cadbc666
nikunjmathur08 <nikunjmathur08@users.noreply.github.com>
2024-07-05 09:54:47 +0530

    Added 03-mongo

--- week-3/03-mongo/db/index.js
+++ week-3/03-mongo/db/index.js
@@ -1,36 +1,36 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://nikunjmathur0810:nIkUnj0810@cluster0.vep0fdl.mongodb.net/course_assignment');
+mongoose.connect("YOUR MONGO DB URL");
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
-    username: {type: String, unique: true, required: true},
-    password: {type: String, required: true},
-    courses: [{type: mongoose.Schema.Types.ObjectId, ref: "Course"}]
+  username: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  courses: [{ type: mongoose.Schema.Types.ObjectId, ref: "Course" }],
 });
 
 const UserSchema = new mongoose.Schema({
-    username: {type: String, unique: true, required: true},
-    password: {type: String, required: true},
-    myCourses: [{type: mongoose.Schema.Types.ObjectId, ref: "Course"}]
+  username: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  myCourses: [{ type: mongoose.Schema.Types.ObjectId, ref: "Course" }],
 });
 
 const CourseSchema = new mongoose.Schema({
-    title: String,
-    description: String,
-    price: Number,
-    image: String,
-    owner: String,
-    published: { type: Boolean, default: false}
+  title: String,
+  description: String,
+  price: Number,
+  image: String,
+  owner: String,
+  published: { type: Boolean, default: false },
 });
 
-const Admin = mongoose.model('Admin', AdminSchema);
-const User = mongoose.model('User', UserSchema);
-const Course = mongoose.model('Course', CourseSchema);
+const Admin = mongoose.model("Admin", AdminSchema);
+const User = mongoose.model("User", UserSchema);
+const Course = mongoose.model("Course", CourseSchema);
 
 module.exports = {
-    Admin,
-    User,
-    Course
-}
+  Admin,
+  User,
+  Course,
+};
