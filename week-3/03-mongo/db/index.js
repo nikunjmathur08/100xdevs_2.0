7584345eec66175ba389645e1cd4398af26583e8
nikunjmathur08 <nikunjmathur08@users.noreply.github.com>
2024-07-05 07:44:20 +0530

    Added 03-mongo assignment

--- week-3/03-mongo/db/index.js
+++ week-3/03-mongo/db/index.js
@@ -1,19 +1,28 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect('mongodb+srv://nikunjmathur0810:nIkUnj0810@cluster0.vep0fdl.mongodb.net/course_assignment');
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
-    // Schema definition here
+    username: {type: String, unique: true, required: true},
+    password: {type: String, required: true},
+    courses: [{type: mongoose.Schema.Types.ObjectId, ref: "Course"}]
 });
 
 const UserSchema = new mongoose.Schema({
-    // Schema definition here
+    username: {type: String, unique: true, required: true},
+    password: {type: String, required: true},
+    myCourses: [{type: mongoose.Schema.Types.ObjectId, ref: "Course"}]
 });
 
 const CourseSchema = new mongoose.Schema({
-    // Schema definition here
+    title: String,
+    description: String,
+    price: Number,
+    image: String,
+    owner: String,
+    published: { type: Boolean, default: false}
 });
 
 const Admin = mongoose.model('Admin', AdminSchema);
